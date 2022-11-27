09f6291856fdef4998a22e994e7e9b28ea75c70f
tiger <duongvuthai2018@gmail.com>
2022-11-27 22:31:08 +0700

    hơmework

---
+++ index.js
@@ -0,0 +1,15 @@
+//bai1
+// function mySon(x){
+//     if(x % 100 == 0) {
+//         x=x/100;
+//     } else {
+//         x=x/100 + 1;
+//     }
+//     return (x);
+// }
+// console.log(Math.floor(mySon(1021)));
+//bai2
+mySon(["xin","chào","mình","tên","là"])
+function mySon(x){
+    console.log(x.CharAt())
+}
